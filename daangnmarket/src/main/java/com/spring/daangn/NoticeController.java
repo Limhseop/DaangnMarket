43fa37f2047e970c0442c82948c09c8eac3cfdca
xxx-xz <torgl1598@gmail.com>
2021-07-07 17:21:31 +0900

    수정

--- daangnmarket/src/main/java/com/spring/daangn/NoticeController.java
+++ daangnmarket/src/main/java/com/spring/daangn/NoticeController.java
@@ -1,8 +1,10 @@
 package com.spring.daangn;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class NoticeController {
 	
 	@RequestMapping(value="/notice.do", method = RequestMethod.GET)
