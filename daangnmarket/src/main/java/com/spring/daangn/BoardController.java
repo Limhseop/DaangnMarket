cea3f9bc7bc5171fb6fb7f5564d94a6fd7a05bd2
dwon <72027383+Dwonii@users.noreply.github.com>
2021-07-09 02:35:01 +0900

    Update BoardController.java
    게시글 추가, 게시글, 게시글 수정
    추가

--- daangnmarket/src/main/java/com/spring/daangn/BoardController.java
+++ daangnmarket/src/main/java/com/spring/daangn/BoardController.java
@@ -7,15 +7,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class BoardController {
 
-	//boardlist-게시판
+	// boardlist-게시판
 	@RequestMapping(value = "/boardlist.do", method = RequestMethod.GET)
 	public String boardlist() {
 		return "/board/boardlist";
 	}
-	
-	//boardPost-게시글
+
+	// board_add-게시글 추가
 	@RequestMapping(value = "/board_add.do", method = RequestMethod.GET)
-	public String boardPost() {
+	public String boardAdd() {
 		return "/board/board_add";
+	}
+
+	// board_post-게시글
+	@RequestMapping(value = "/board_post.do", method = RequestMethod.GET)
+	public String boardPost() {
+		return "/board/board_post";
+	}
+	
+	// board_update-게시글 수정
+	@RequestMapping(value = "/board_update.do", method = RequestMethod.GET)
+	public String boardUpdate() {
+		return "/board/board_update";
 	}
 }
