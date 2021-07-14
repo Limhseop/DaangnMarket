935a932c51397081e41c12f9b1c0fc0463430dab
jiwon2581 <82023207+jiwon2581@users.noreply.github.com>
2021-07-15 00:49:04 +0900

    커밋
    다한건 아니고요 오류가 있어서 커밋합니다.

--- daangnmarket/src/main/java/com/spring/dao/ProductDAO.java
+++ daangnmarket/src/main/java/com/spring/dao/ProductDAO.java
@@ -1,8 +1,98 @@
 package com.spring.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.spring.vo.ProductVO;
+
+@Repository
 public class ProductDAO {
-	//insert : 게시판 글 쓰기
-	public boolean getInsertResult(prdouctVO) {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	private static String namespace = "mapper.product";
+	
+	//select : 행 수 얻어오기
+	public int execTotalCount() {
+		return sqlSession.selectOne(namespace+".count");
+	}
+	
+	//select : 파일 이름 가져오기
+	public String getPsfile(String pid) {
+		return sqlSession.selectOne(namespace+".psfile", pid);
 		
+	}
+	
+	//delete : 삭제하기
+	public boolean getDeleteResult(String pid) {
+		boolean result = false;
+		int value = sqlSession.delete(namespace+".delete", pid);
+		if(value != 0) {
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	//update : 수정하기(파일 미포함)
+	public boolean getUpdateResultNofile(ProductVO vo) {
+		boolean result = false;
+		int value = sqlSession.update(namespace+".updateNofile", vo);
+		if(value != 0) {
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	//update : 수정하기 (파일 포함)
+	public boolean getUpdateResult(ProductVO vo) {
+		boolean result = false;
+		int value = sqlSession.update(namespace+".update", vo);
+		if(value != 0) {
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	//update: 조회수 올리기
+	public void getUpdateHit(String pid) {
+		sqlSession.update(namespace+".phit", pid);
+	}
+	
+	//select : 상세내용 가져오기
+	public ProductVO getContent(String pid) {
+		return sqlSession.selectOne(namespace+".content", pid);
+	}
+	
+	//select: 리스트 가져오기
+	public ArrayList<ProductVO> getList(int start, int end){
+		Map<String, Object> param = new HashMap();
+		param.put("start", start);
+		param.put("end", end);
+		
+		List<ProductVO> list = sqlSession.selectList(namespace+".productlist", param);
+		
+		return (ArrayList<ProductVO>)list;
+	}
+	
+	//insert : 게시판 글 쓰기
+	public boolean getInsertResult(ProductVO vo) {
+		boolean result = false;
+		
+		int value = sqlSession.insert(namespace+".insert", vo);
+		
+		if(value != 0) {
+			result = true;
+		}
+		
+		return result;
 	}
 }
--- daangnmarket/src/main/java/com/spring/vo/ProductVO.java
+++ daangnmarket/src/main/java/com/spring/vo/ProductVO.java
@@ -1,8 +1,72 @@
 package com.spring.vo;
 
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
 public class ProductVO {
-	int rno, phit;
-	String pid, ptitle, pcontent, pdate, pfile, psfile;
+	int rno, phit, price, pchoice, chat, favorite, report;
+	String pid, ptitle, pcontent, pdate, pfile, psfile, category, location, id, saled;
+	CommonsMultipartFile pfile1;
+	
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getPchoice() {
+		return pchoice;
+	}
+	public void setPchoice(int pchoice) {
+		this.pchoice = pchoice;
+	}
+	public int getChat() {
+		return chat;
+	}
+	public void setChat(int chat) {
+		this.chat = chat;
+	}
+	public int getFavorite() {
+		return favorite;
+	}
+	public void setFavorite(int favorite) {
+		this.favorite = favorite;
+	}
+	public int getReport() {
+		return report;
+	}
+	public void setReport(int report) {
+		this.report = report;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSaled() {
+		return saled;
+	}
+	public void setSaled(String saled) {
+		this.saled = saled;
+	}
+	public CommonsMultipartFile getPfile1() {
+		return pfile1;
+	}
+	public void setPfile1(CommonsMultipartFile pfile1) {
+		this.pfile1 = pfile1;
+	}
 	public int getRno() {
 		return rno;
 	}
