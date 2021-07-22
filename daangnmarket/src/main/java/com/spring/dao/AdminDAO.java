a72e4ef94eb5d96b6a30805e36f379e22be63a3b
김세희 <82367039+sadie100@users.noreply.github.com>
2021-07-22 17:21:36 +0900

    commit

--- daangnmarket/src/main/java/com/spring/dao/AdminDAO.java
+++ daangnmarket/src/main/java/com/spring/dao/AdminDAO.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.spring.vo.MemberVO;
 
+@Repository
 public class AdminDAO {
 	
 	@Autowired
